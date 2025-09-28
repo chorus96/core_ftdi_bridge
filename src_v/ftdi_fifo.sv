2fab37d1662567e03bb73505133915c8c9f6aba7
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-09-28 12:08:16 +0000

    Study

--- src_v/ftdi_fifo.sv
+++ src_v/ftdi_fifo.sv
@@ -6,55 +6,51 @@ module ftdi_fifo #(
     ,parameter ADDR_W  = 6
     ,parameter COUNT_W = 7
 )(
-     input                clk_i
-    ,input                rst_i   
-    ,input  [WIDTH-1:0]   data_in_i
-    ,input                push_i
-    ,input                pop_i  
-    ,output [WIDTH-1:0]   data_out_o
-    ,output               accept_o
-    ,output               valid_o
-    ,output [COUNT_W-1:0] level_o
+     input  logic               clk_i
+    ,input  logic               rst_i   
+    ,input  logic [WIDTH-1:0]   data_in_i
+    ,input  logic               push_i
+    ,input  logic               pop_i  
+    ,output logic [WIDTH-1:0]   data_out_o
+    ,output logic               accept_o
+    ,output logic               valid_o
+    ,output logic [COUNT_W-1:0] level_o
 );
 
 /* Registers */
-reg [WIDTH-1:0]   ram_q[DEPTH-1:0];
-reg [ADDR_W-1:0]  rd_ptr_q;
-reg [ADDR_W-1:0]  wr_ptr_q;
+reg [  WIDTH-1:0] ram_q[DEPTH-1:0];
+reg [ ADDR_W-1:0] rd_ptr_q;
+reg [ ADDR_W-1:0] wr_ptr_q;
 reg [COUNT_W-1:0] count_q;
 
-/* Sequential */
-always @ (posedge clk_i or posedge rst_i)
+always_ff @ (posedge clk_i or posedge rst_i)
 if (rst_i) begin
     count_q   <= {(COUNT_W) {1'b0}};
-    rd_ptr_q  <= {(ADDR_W) {1'b0}};
-    wr_ptr_q  <= {(ADDR_W) {1'b0}};
+    rd_ptr_q  <= {( ADDR_W) {1'b0}};
+    wr_ptr_q  <= {( ADDR_W) {1'b0}};
 end else begin
-    // Push
-    if (push_i & accept_o) begin
+    if (push_i & accept_o) begin // Push
         ram_q[wr_ptr_q] <= data_in_i;
         wr_ptr_q        <= wr_ptr_q + 1;
     end
+    
+    if (pop_i & valid_o) // Pop
+        rd_ptr_q <= rd_ptr_q + 1;
 
-    // Pop
-    if (pop_i & valid_o)
-        rd_ptr_q      <= rd_ptr_q + 1;
-
-    // Count up
-    if ((push_i & accept_o) & ~(pop_i & valid_o))
+    if ((push_i & accept_o) & ~(pop_i & valid_o)) // Count up
         count_q <= count_q + 1;
-    // Count down
-    else if (~(push_i & accept_o) & (pop_i & valid_o))
+    else if (~(push_i & accept_o) & (pop_i & valid_o)) // Count down
         count_q <= count_q - 1;
 end
 
-/* Combinatorial */
-/* verilator lint_off WIDTH */
-assign valid_o       = (count_q != 0);
-assign accept_o      = (count_q != DEPTH);
-/* verilator lint_on WIDTH */
+always_comb begin : out_block
+    /* verilator lint_off WIDTH */
+    valid_o       = (count_q != 0);
+    accept_o      = (count_q != DEPTH);
+    /* verilator lint_on WIDTH */
 
-assign data_out_o    = ram_q[rd_ptr_q];
-assign level_o       = count_q;
+    data_out_o    = ram_q[rd_ptr_q];
+    level_o       = count_q;
+end
 
 endmodule
